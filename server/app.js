40f35cf412c2456c89efc050dfd8f4d97701b041
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-08-31 03:48:56 +0900

    server: mongoose connect

--- server/app.js
+++ server/app.js
@@ -1,19 +1,56 @@
 const express = require('express');
 const app = express();
+const { connect: dbConnect } = require('./models');
+const ProductModel = require('./models/product');
 
-app.get('/product', function (req, res) {
-  res.send('Get Product');
+dbConnect();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/product', async function (req, res) {
+  const products = await ProductModel.find({}).exec();
+  res.send(products);
 });
 
-app.post('/product', function (req, res) {
+app.get('/product/:id', async function (req, res) {
+  const id = req.params.id;
+  const products = await ProductModel.findOne({ _id: id }).exec();
+  res.send(products);
+});
+
+app.post('/product', async function (req, res) {
+  const { title, imgLink, detail, price } = req.body;
+  await ProductModel.create({
+    title,
+    imgLink,
+    detail,
+    price,
+  });
   res.send('Post Product');
 });
 
-app.put('/product', function (req, res) {
-  res.send('Put Product');
+app.put('/product', async function (req, res) {
+  const { id, title, imgLink, detail, price } = req.body;
+  await ProductModel.updateOne(
+    { _id: id },
+    {
+      title,
+      imgLink,
+      detail,
+      price,
+    }
+  );
+  res.send('Update Product');
 });
 
-app.delete('/product', function (req, res) {
+app.delete('/product/:id', async function (req, res) {
+  const id = req.params.id;
+  await ProductModel.deleteOne({ _id: id }).exec();
   res.send('Delete Product');
 });
 
---
+++ server/models/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { getDBUri } = require('../config');
+
+let db;
+const connect = async () => {
+  const DB_URI = getDBUri();
+
+  if (db) {
+    return;
+  }
+  mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+  });
+
+  db = mongoose.connection;
+};
+
+const disconnect = () => {
+  if (!db) {
+    return;
+  }
+  mongoose.disconnect();
+};
+
+module.exports = { connect, disconnect };
