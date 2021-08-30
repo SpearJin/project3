368de00ba7642a2c63fb4d3c2cb17049eddca2b4
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-08-30 18:50:55 +0900

    FontAwesome

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,13 @@ function Cart() {
   return (
     <>
       <span>책상</span>
-      <button>+</button>
+      <button>
+        <i className='fas fa-plus-square'></i>
+      </button>
       <span>0</span>
-      <button>-</button>
+      <button>
+        <i className='fas fa-minus-square'></i>
+      </button>
       <span>30000원</span>
     </>
   );
