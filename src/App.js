ef190a143a6282f8661c2b37a6f1af66bcea7765
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-08-30 23:13:28 +0900

    ProductInfo: Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Cart from './components/Cart';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ProductInfo from './components/ProductInfo';
 
 function App() {
   return (
     <>
       <Header />
-      <Cart />
+      <ProductInfo />
       <Footer />
     </>
   );
